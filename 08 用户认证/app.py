d31475a8a9be18a6edf88b7b3f97f4a3a2637ce6
bosom <1245924849@qq.com>
2023-08-20 17:48:47 +0800

    用户认证

--- 08 用户认证/app.py
+++ 08 用户认证/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
+import click
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    logout_user,
+    login_required,
+    current_user,
+)
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecretkey"
@@ -9,13 +20,26 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # 关闭对模型修改的监控
 
+
 db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message = "需要登录"
 
 
 # 创建数据库模型
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):  # 表名将会是 movie
@@ -24,8 +48,10 @@ class Movie(db.Model):  # 表名将会是 movie
     year = db.Column(db.String(4))  # 电影年份
 
 
-def create_data():
+@app.cli.command()
+def init():
     # 第一次使用
+    db.drop_all()
     db.create_all()
     name = "bosom"
     movies = [
@@ -50,9 +76,88 @@ def create_data():
     print("添加完成")
 
 
+# 生成管理员账户
+@app.cli.command()
+@click.option("--username", prompt=True, help="The username used to login.")
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    confirmation_prompt=True,
+    help="The password used to login.",
+)
+def admin(username, password):
+    user = User.query.first()
+
+    if user is not None:
+        click.echo("更新管理员")
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo("创建管理员")
+        user = User(username=username, name="Admin")
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo("完成创建")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.first()
+        if not username or not password or len(password) < 6:
+            flash("格式错误")
+        elif user.username == username and user.validate_password(password):
+            login_user(user)
+            flash("登录成功")
+            return redirect(url_for("index"))
+        else:
+            flash("账号或密码错误")
+
+    return render_template("login.html")
+
+
+# 账号登出
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("退出账号")
+    return redirect(url_for("index"))
+
+
+# 修改用户的名字
+@app.route("/setting", methods=["GET", "POST"])
+@login_required
+def setting():
+    if request.method == "POST":
+        name = request.form.get("name")
+        if not name or len(name) < 3 or len(name) > 20:
+            flash("名称不符合规范")
+            return redirect(url_for("setting"))
+        else:
+            current_user.name = name
+            db.session.commit()
+            flash("更新成功")
+            return redirect(url_for("index"))
+    return render_template("setting.html")
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        if not current_user.is_authenticated:  # 如果当前用户未认证
+            flash("请登录")
+            return redirect(url_for("index"))  # 重定向到主页
         title = request.form.get("title")
         year = request.form.get("year")
         if not title or not year or len(year) < 4 or len(title) > 60:
@@ -70,6 +175,7 @@ def index():
 
 # 编辑条目
 @app.route("/movie/edit/<int:movie_id>", methods=["GET", "POST"])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     if request.method == "POST":
@@ -90,6 +196,7 @@ def edit(movie_id):
 
 # 删除条目
 @app.route("/movie/delete/<int:movie_id>", methods=["GET", "POST"])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
