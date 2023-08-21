413ca37302a4937ed1aa9b4f2932e65d9c134645
bosom <1245924849@qq.com>
2023-08-21 17:21:33 +0800

    代码重构

--- 10 代码重构/单元测试/hello.py
+++ 10 代码重构/单元测试/hello.py
@@ -1,7 +1,12 @@
+import os
+
+
 def sayhello(to=None):
     if to:
         return f"Hello, {to}!"
     return "Hello!"
 
 
+SECRET_KEY = "1111"
 print(dict(title="New Movie", year="2019"))
+print(os.getenv("SECRET_KEY"))
